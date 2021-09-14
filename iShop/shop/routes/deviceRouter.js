5617ef83164e643b6011709f706e51849d7cc91e
Wladiosan <w.ohrimenko@gmail.com>
2021-09-14 19:57:28 +0300

    add back-end part for shop

---
+++ iShop/shop/routes/deviceRouter.js
@@ -0,0 +1,11 @@
+const Router = require('express')
+
+const DeviceController = require('../controllers/deviceController')
+
+const router = new Router()
+
+router.post('/', DeviceController.create)
+router.get('/', DeviceController.getAll)
+router.get('/:id', DeviceController.getOne)
+
+module.exports = router
