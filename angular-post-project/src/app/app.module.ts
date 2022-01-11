fae0fad697b8ab8a3b5664961c8c5c4a6eacd468
Wladiosan <w.ohrimenko@gmail.com>
2022-01-11 03:34:40 +0200

    fixed bug

--- angular-post-project/src/app/app.module.ts
+++ angular-post-project/src/app/app.module.ts
@@ -2,13 +2,11 @@ import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 
 import {AppRoutingModule} from './app-routing.module'
-import {AppComponent} from './app.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component'
+import {AppComponent} from './app.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainLayoutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
