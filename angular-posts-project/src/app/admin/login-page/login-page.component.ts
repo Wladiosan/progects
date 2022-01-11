fe49a657877b46900ce0478da2170237ec62acf3
Wladiosan <w.ohrimenko@gmail.com>
2022-01-11 06:54:35 +0200

    create interface for User data

--- angular-posts-project/src/app/admin/login-page/login-page.component.ts
+++ angular-posts-project/src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import {FormControl, FormGroup, Validators} from "@angular/forms"
 
+import {User} from '../../shared/interfaces'
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -37,9 +39,13 @@ export class LoginPageComponent implements OnInit {
 
 
   submit() {
-    console.log(this.form)
     if (this.form.invalid) {
       return
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
     }
   }
 }
